149e506687bfa9b0d32eba4cd785906f6a16ab0e
Michael <Matvi>
2013-12-26 10:48:11 +0200

    Created new project

---
+++ PhoneApp1/PhoneApp1/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhoneApp1
+{
+    public class DataAccess
+    {
+        public static List<Model> GetModels()
+        {
+            var list = new List<Model>
+                {
+                    new Model {Name = "name0", Age = 0},
+                    new Model {Name = "name1", Age = 1},
+                    new Model {Name = "name2", Age = 2},
+                    new Model {Name = "name3", Age = 3},
+                    new Model {Name = "name4", Age = 4},
+                };
+            return list;
+        }
+
+        public static List<string> GetNames()
+        {
+            var list = new List<string> { "name0", "name1", "name2", "name3", "name4" };
+            return list;
+        }
+    }
+}
---
+++ PhoneApp1/PhoneApp1/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace PhoneApp1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Button2.Click += Button2_Click;
+            foreach (string name in DataAccess.GetNames()){
+                var listBoxNew = new ListBoxItem()
+                {
+                    Content = name,
+                    FontSize = 24,
+                };
+                ListBox1.Items.Add(listBoxNew);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            TextBlock1.Text = ("You have pressed: " + button.Name);
+        }
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBox2.Text != "")
+            {
+                var listBoxNew = new ListBoxItem()
+                {
+                    Content = TextBox2.Text,
+                    FontSize = 24,
+                };
+                ListBox1.Items.Add(listBoxNew);
+            }
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show("List Item pressed : " + ((ListBoxItem)ListBox1.Items[ListBox1.SelectedIndex]).Content);
+        }
+    }
+}
